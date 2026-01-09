93d280762e0bfce302c37feec273eec74e790448
Ainhoa Marcos <ies.teis.1smr.ainhoa@gmail.com>
2026-01-09 09:47:05 +0100

    Reorganización final!!!

--- backend/router/index.js
+++ backend/router/index.js
@@ -1,16 +1,16 @@
-import NotFound from '../components/NotFound.vue'
-import PaginaInicio from '../components/PaginaInicio.vue'
-import GestionClientes from '../components/GestionClientes.vue'
-import NotiCias from '../components/NotiCias.vue';
+import NotFound from '@/components/NotFound.vue'
+import PaginaInicio from '@/components/PaginaInicio.vue'
+import GestionClientes from '@/components/GestionClientes.vue'
+import NotiCias from '@/components/NotiCias.vue';
 import { createRouter, createWebHistory } from "vue-router";
-import AvisoLegal from '../components/AvisoLegal.vue';
-import PoliticaPrivacidad from '../components/PoliticaPrivacidad.vue';
-import MoDelos from '../components/MoDelos.vue';  
-import CitasTaller from '../components/CitasTaller.vue';
-import TablaLogin from '../components/TablaLogin.vue';
-import VenTas from '../components/VenTas.vue';
-import ConTacto from '../components/ConTacto.vue';
-import BusCar from '../components/BusCar.vue';
+import AvisoLegal from '@/components/AvisoLegal.vue';
+import PoliticaPrivacidad from '@/components/PoliticaPrivacidad.vue';
+import MoDelos from '@/components/MoDelos.vue';  
+import CitasTaller from '@/components/CitasTaller.vue';
+import TablaLogin from '@/components/TablaLogin.vue';
+import VenTas from '@/components/VenTas.vue';
+import ConTacto from '@/components/ConTacto.vue';
+import BusCar from '@/components/BusCar.vue';
 
 
 
